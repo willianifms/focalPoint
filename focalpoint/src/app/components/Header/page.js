5444c4dfe2bee666b01d03913727c544ae97ff5e
willian <williancardoso00004@gmail.com>
2024-09-19 14:48:39 -0400

    Feat: feature Header

---
+++ focalpoint/src/app/components/Header/page.js
@@ -0,0 +1,47 @@
+
+import styles from "./header.module.scss"
+import Image from 'next/image'
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
+
+moment.locale('pt-br')
+
+let now = moment().format('LLLL').slice(0,36);
+
+export default function Header(){
+
+return(
+<header className={styles.header}>
+
+    
+<section className = {styles.logo}>
+<Image
+src="/images/shape.png"
+alt="Picture of the author"
+width={33.1}
+height={32.91}
+/>
+<Image
+src="/images/logotype.png"
+alt="Picture of the author"
+width={106}
+height={15}
+className={styles.logoType}
+/>
+</section>
+
+<section>
+    <p className={styles.welcome}>Bem-Vindo de volta, Marcus</p> 
+</section>
+
+<section> 
+    <p className={styles.date}>{now}</p>
+</section>
+</header>
+
+)
+
+
+
+
+}
---
+++ focalpoint/src/app/page.js
@@ -0,0 +1,6 @@
+import Header from "./components/Header/page";
+export default function Home() {
+  return (
+    <Header/>
+  );
+}
