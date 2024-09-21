b9f9f51344ed6f308384b5e460067795794b57bb
willian <williancardoso00004@gmail.com>
2024-09-21 10:34:06 -0400

    Fix: use client from pages

--- focalpoint/src/app/components/DeletarTarefa/page.js
+++ focalpoint/src/app/components/DeletarTarefa/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import React from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
--- focalpoint/src/app/components/itemTarefa/page.js
+++ focalpoint/src/app/components/itemTarefa/page.js
@@ -1,3 +1,4 @@
+'use client'
 import styles from "./style.module.scss";
 import DeletarTarefa from "../DeletarTarefa/page";
 
